781a63b39992e9e1497b4c581da1a12b0490388b
hoadt <jevermore95@gmail.com>
2025-11-05 01:54:32 +0700

    hihihi

--- src/sample/blankMap.js
+++ src/sample/blankMap.js
@@ -17,3 +17,12 @@ export default [
   ['W', N , N , N , N , N , N , N , N , N , N , N , N , N , N ,'W'],
   ['W','W','W','W','W','W','W','W','W','W','W','W','W','W','W','W']
 ]
+
+// SOCKET_SERVER=https://zarena-dev7.zinza.com.vn
+// TOKEN=z4mbx2h7
+// BOMBER_NAME=bobao
+// TOKEN2=t7dw9y4l
+// BOMBER_NAME2=bobao(1)
+// TOKEN3=c3nv8j5r
+// BOMBER_NAME3=aka000
+// ENV=local
