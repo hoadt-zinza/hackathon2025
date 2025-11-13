5e03ad42b95b8c920100d1d6af4c636302ff6970
hoadt <jevermore95@gmail.com>
2025-11-13 07:15:18 +0700

    add MapMoc

---
+++ src/sample/mapMoc.js
@@ -0,0 +1,19 @@
+const N = null
+export default [
+    ["W","W","W","W","W","W","W","W","W","W","W","W","W","W","W","W"],
+    ["W", N , N ,"C", N ,"C", N ,"W","W", N ,"C", N ,"C", N , N ,"W"],
+    ["W", N , N ,"C","W", N ,"C", N , N ,"C", N ,"W","C", N , N ,"W"],
+    ["W","C","C","C", N ,"C", N ,"C","C", N ,"C", N ,"C","C","C","W"],
+    ["W", N ,"W", N ,"C", N ,"W", N , N ,"W", N ,"C", N ,"W", N ,"W"],
+    ["W","C", N ,"C", N ,"C", N ,"C","C", N ,"C", N ,"C", N ,"C","W"],
+    ["W", N ,"C", N ,"W", N ,"C", N , N ,"C", N ,"W", N ,"C", N ,"W"],
+    ["W","W","C","C", N ,"C", N , N ,"W", N ,"C", N ,"C","C","W","W"],
+    ["W","W","C","C", N ,"C", N ,"W", N , N ,"C", N ,"C","C","W","W"],
+    ["W", N ,"C", N ,"W", N ,"C", N , N ,"C", N ,"W", N ,"C", N ,"W"],
+    ["W","C", N ,"C", N ,"C", N ,"C","C", N ,"C", N ,"C", N ,"C","W"],
+    ["W", N ,"W", N ,"C", N ,"W", N , N ,"W", N ,"C", N ,"W", N ,"W"],
+    ["W","C","C","C", N ,"C", N ,"C","C", N ,"C", N ,"C","C","C","W"],
+    ["W", N , N ,"C","W", N ,"C", N , N ,"C", N ,"W","C", N , N ,"W"],
+    ["W", N , N ,"C", N ,"C", N ,"W","W", N ,"C", N ,"C", N , N ,"W"],
+    ["W","W","W","W","W","W","W","W","W","W","W","W","W","W","W","W"]
+]
